89352574a04219de6a14af69ecb9d4edba004303
bakhtawar7 <bakhtawarfatima34q@gmail.com>
2026-06-21 12:58:26 +0500

    backend ready for render

--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Backend is running"
+
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
